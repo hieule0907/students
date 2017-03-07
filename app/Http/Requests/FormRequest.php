15862f663c936c3d069812370507174c4d1b61f4
hieule0907 <hieulm@appota.com>
2017-03-07 15:47:58 +0700

    Update validation message

--- app/Http/Requests/FormRequest.php
+++ app/Http/Requests/FormRequest.php
@@ -33,4 +33,27 @@ class FormRequest extends Request
             'studentBirthday' => 'required|date'
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'studentName.required' => 'Tên sinh viên không được để trống',
+            'studentId.required'  => 'Mã SV không được để trống',
+            'studentEmail.required' => 'Email không được để trống',
+            'studentEmail.email' => 'Email không hợp lệ',
+            'studentClass.required' => 'Hãy chọn lớp',
+            'studentGender.required' => 'Hãy chọn giới tính',
+            'studentPhone.required' => 'SĐT không được để trống',
+            'studentPhone.numeric' => 'SĐT không hợp lệ',
+            'studentBirthday.required' => 'Ngày sinh không được để trống',
+            'studentBirthday.date' => 'Ngày sinh không hợp lệ'
+        ];
+}
+
+
 }
